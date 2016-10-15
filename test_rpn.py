78b148c1a09de5bae940953ac9c4006548f07040
chboggs <chboggs@umich.edu>
2016-10-15 17:08:44 -0400

    Added testing for exp

--- test_rpn.py
+++ test_rpn.py
@@ -18,3 +18,6 @@ class TestBasics(unittest.TestCase):
 	def test_toomanythings(self):
 		with self.assertRaises(TypeError):
 			rpn.calculate("1 2 3 +")
+	def test_exp(self):
+		result = rpn.calculate("5 2 ^")
+		self.assertEqual(25, result)
